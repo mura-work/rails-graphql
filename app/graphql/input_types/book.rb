cfb1324e2c98347cff163195b12a06169e0623d5
murata-naoya <murata.small.baseball@gmail.com>
2022-04-30 13:52:42 +0900

    mutationの作成

--- app/graphql/input_types/book.rb
+++ app/graphql/input_types/book.rb
@@ -2,4 +2,5 @@ class InputTypes::Book < Types::BaseInputObject
   graphql_name 'BookAttributes' # ObjectTypes::Bookと名前がバッティングしないようにする
 
   argument :title, String, required: true
+  argument :body, String, required: true
 end
